20b857759043110622787493e4d12c2ee5c7d60c
Berry  Semexan <bsemexan@gmail.com>
2018-03-01 02:55:42 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,7 +1,7 @@
 # Create your costume_stores migration here
 class CostumeStores < ActiveRecord::Migration
   def change
-    create_table :costumes do |t|
+    create_table :costume_stores do |t|
       t.string :name
       t.string :location
       t.integer :costume_inventory
