3ab579447fafd34c22b78ff2ff046e26ca96a304
Berry  Semexan <bsemexan@gmail.com>
2018-03-01 02:54:41 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
 # Create your haunted_houses migration here
+class HauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.timestamps
+    end
+  end
+end
