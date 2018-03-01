ee9da50198ee6eb688f5150ff2c5c46b5ed9b799
Berry  Semexan <bsemexan@gmail.com>
2018-03-01 03:09:33 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string    :theme
       t.integer   :price
       t.boolean   :family_friendly
-      t.string      :description
+      t.string    :description
       t.datetime  :opening_date
       t.datetime  :closing_date
       t.timestamps
